0648e3f87ce625bc528f887deced2364500793c6
Diego Pizzo <>
2021-12-11 14:33:12 +0000

    add data model

---
+++ network/src/main/java/com/diegopizzo/network/model/PropertiesModel.kt
@@ -0,0 +1,19 @@
+package com.diegopizzo.network.model
+
+/**
+ * I don't have any information about the response model in terms of optional/nullable attributes.
+ * So I assume that all of it can be null.
+ */
+internal data class Property(
+    val id: Long? = null,
+    val price: Int? = null,
+    val bedrooms: Int? = null,
+    val bathrooms: Int? = null,
+    val number: String? = null,
+    val address: String? = null,
+    val region: String? = null,
+    val postcode: String? = null,
+    val propertyType: String? = null
+)
+
+internal data class Properties(val properties: List<Property>)
