79180ee4bbded7f368eadbed63d1444ce0399787
Diego Pizzo <>
2021-12-11 17:25:56 +0000

    add interactor and unit tests

---
+++ network/src/main/java/com/diegopizzo/network/interactor/PropertiesInteractor.kt
@@ -0,0 +1,25 @@
+package com.diegopizzo.network.interactor
+
+import com.diegopizzo.network.base.BaseInteractor
+import com.diegopizzo.network.base.Result
+import com.diegopizzo.network.creator.PropertiesCreator
+import com.diegopizzo.network.model.AveragePrice
+import com.diegopizzo.network.service.ApiService
+import io.reactivex.Single
+
+internal class PropertiesInteractor(
+    private val apiService: ApiService,
+    private val creator: PropertiesCreator
+) : BaseInteractor(), IPropertiesInteractor {
+
+    override fun getAveragePropertyPrice(): Single<Result<AveragePrice>> {
+        return apiService.getProperties().map {
+            when (val result = handleResponse(it)) {
+                is Result.Success -> Result.Success(creator.getAveragePrice(result.data.properties))
+                is Result.Error -> Result.Error(result.errorCode)
+            }
+        }.onErrorReturn {
+            Result.Error()
+        }
+    }
+}
